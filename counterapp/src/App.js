0f35ec75929dc3b65c5a563f5475c88d69359532
logesh-waran-p <logesh.7871ll@gmail.com>
2024-08-13 14:11:47 +0530

    update counterapp using react

--- counterapp/src/App.js
+++ counterapp/src/App.js
@@ -15,15 +15,47 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
+  function reset() {
+    setStep(0);
+    setCount(1);
+  }
+
   return (
     <div className="count">
-      <button onClick={() => setStep((step) => step - 1)}>-</button>
+      <input
+        type="range"
+        min="1"
+        max="10"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
+      <span>{step}</span>
+      {/* <button onClick={() => setStep((step) => step - 1)}>-</button>
       <span>Step: {step}</span>
-      <button onClick={() => setStep((step) => step + 1)}>+</button>
+      <button onClick={() => setStep((step) => step + 1)}>+</button> */}
       <br />
-      <button onClick={() => setCount((count) => count - step)}>-</button>
-      <span>Count: {count}</span>
-      <button onClick={() => setCount((count) => count + step)}>+</button>
+      <div>
+        <button
+          className="btn"
+          onClick={() => setCount((count) => count - step)}
+        >
+          -
+        </button>
+
+        <input
+          className="btn"
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+
+        <button
+          className="btn"
+          onClick={() => setCount((count) => count + step)}
+        >
+          +
+        </button>
+      </div>
       <p>
         <span>
           {count === 0
@@ -33,6 +65,12 @@ function Counter() {
             : `${absoluteValue} days ago was ${date.toDateString()}`}
         </span>
       </p>
+
+      {(count > 0 || step > 1) && (
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+      )}
     </div>
   );
 }
